160035a5eeb5f24cb7ca121ef0a78e465381826f
Codenetz <codenetz.mail@gmail.com>
2019-04-24 23:59:08 +0300

    add .npmignore

--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
 import { expect } from 'chai';
 import isRemote from '../src/index';
 
+const localFile = 'bar.png';
+const remoteFile = 'https://raw.githubusercontent.com/Codenetz/is-remote/master/test/bar.png';
+
 describe('#remote', () => {
   it('should return false', () => {
     expect(isRemote('bar.png')).to.equal(false);
