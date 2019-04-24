c3931d323e40f5d8118d0706d389ff03ec359218
Codenetz <codenetz.mail@gmail.com>
2019-04-25 01:16:31 +0300

    add path checking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-export default path => {
-  if (!path.startsWith('http') || !path.startsWith('www')) return false;
+import http from 'http';
+import https from 'https';
 
-  console.log('pass', path);
-  return false;
+export default (path, validStatusCodes = [200]) => {
+  return new Promise(resolve => {
+    if (!path.startsWith('http')) return resolve(false);
+    try {
+      const protocol = path.startsWith('https') ? https : http;
+      let req = protocol.request(new URL(path), res => {
+        return resolve(validStatusCodes.indexOf(res.statusCode) >= 0);
+      });
+      req.end();
+    } catch (e) {
+      return resolve(false);
+    }
+  });
 };
--- test/index.js
+++ test/index.js
@@ -2,10 +2,65 @@ import { expect } from 'chai';
 import isRemote from '../src/index';
 
 const localFile = 'bar.png';
-const remoteFile = 'https://raw.githubusercontent.com/Codenetz/is-remote/master/test/bar.png';
+const remoteFileHttps =
+  'https://raw.githubusercontent.com/Codenetz/is-remote/master/test/bar.png';
+const remoteFileHttpsMisspelling =
+  'https://raw.githubusercontent.com/Codenetz/is-remote/master/test/barrrrrrr.png';
+const remoteFileHttp =
+  'http://raw.githubusercontent.com/Codenetz/is-remote/master/test/bar.png';
 
 describe('#remote', () => {
-  it('should return false', () => {
-    expect(isRemote('bar.png')).to.equal(false);
+  it('should return false on wrong URL', () => {
+    isRemote('http')
+      .then(res => {
+        expect(res).to.equal(false);
+      })
+      .catch(e => {
+        console.log('[should return false on wrong URL]');
+        console.error(e);
+      });
+  });
+
+  it('should return false on local file', () => {
+    isRemote(localFile)
+      .then(res => {
+        expect(res).to.equal(false);
+      })
+      .catch(e => {
+        console.log('[should return false on local file]');
+        console.error(e);
+      });
+  });
+  it('should return true on remote https file', () => {
+    isRemote(remoteFileHttps)
+      .then(res => {
+        expect(res).to.equal(true);
+      })
+      .catch(e => {
+        console.log('[should return true on remote https file]');
+        console.error(e);
+      });
+  });
+
+  it('should return true on remote http file', () => {
+    isRemote(remoteFileHttp, [200, 301, 302])
+      .then(res => {
+        expect(res).to.equal(true);
+      })
+      .catch(e => {
+        console.log('[should return true on remote http file]');
+        console.error(e);
+      });
+  });
+
+  it('should return false on not found remote file', () => {
+    isRemote(remoteFileHttpsMisspelling)
+      .then(res => {
+        expect(res).to.equal(false);
+      })
+      .catch(e => {
+        console.log('[should return false on not found remote file]');
+        console.error(e);
+      });
   });
 });
